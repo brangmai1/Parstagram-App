5d9ab78820945b99c1256851d364e772fac904b4
Brang Mai <brangmai100@gmail.com>
2021-10-05 17:56:01 -0400

    Sing up and Log in functionalities were added to the project

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "H0SjMW0cnJNUn5p9enOq3khufC5m6hohdJqFIUAA" // <- UPDATE
+                    $0.clientKey = "LODDLLPordPb5igjgloJmuBmrvJwgP8APnIKoESo" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
         return true
     }
 
