d3916aedd2e6baf25f5512d59d7aa5f4f22246fe
Brang Mai <brangmai100@gmail.com>
2021-10-11 15:35:40 -0400

    Added login and logout functionalities

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -69,5 +69,15 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+        let delegate = windowScene.delegate as? SceneDelegate else { return }
+        delegate.window?.rootViewController = loginViewController
+    }
+    
 }
